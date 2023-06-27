152ed267070d5b267088bd49f1fc1a5ddf9e30de
Jonathan Tipton <nature_explorer98@outlook.com>
2023-06-27 13:53:14 -0600

    Initial Commit

---
+++ P5-WordScramble/P5_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  P5_WordScrambleApp.swift
+//  P5-WordScramble
+//
+//  Created by Jonathan Tipton on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct P5_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
