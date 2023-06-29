385e70985fca3727e986986ade6229fd0b43eb11
Jonathan Tipton <nature_explorer98@outlook.com>
2023-06-29 13:48:21 -0600

    Initial Commit

---
+++ C1-Edutainment/C1_EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  C1_EdutainmentApp.swift
+//  C1-Edutainment
+//
+//  Created by Jonathan Tipton on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct C1_EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
