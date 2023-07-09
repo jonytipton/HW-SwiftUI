8148e71485e4fd587d652107d4df743dc6dc2724
Jonathan Tipton <nature_explorer98@outlook.com>
2023-07-08 21:26:28 -0600

    Initial Commit

---
+++ P9-Drawing/P9_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  P9_DrawingApp.swift
+//  P9-Drawing
+//
+//  Created by Jonathan Tipton on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct P9_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
