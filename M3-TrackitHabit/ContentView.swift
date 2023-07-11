95a547563a0bbf6c6e43ec22d679c00f4271169d
Jonathan Tipton <nature_explorer98@outlook.com>
2023-07-11 16:11:39 -0600

    Initial Commit

---
+++ M3-TrackitHabit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  M3-TrackitHabit
+//
+//  Created by Jonathan Tipton on 7/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3-TrackitHabit/M3_TrackitHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3_TrackitHabitApp.swift
+//  M3-TrackitHabit
+//
+//  Created by Jonathan Tipton on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct M3_TrackitHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
