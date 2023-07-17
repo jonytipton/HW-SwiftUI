dab4f87c8223e8f5d025512afff42786cb13bc75
Jonathan Tipton <nature_explorer98@outlook.com>
2023-07-16 19:35:30 -0600

    Initial Commit

---
+++ P10-CupcakeCorner/P10_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  P10_CupcakeCornerApp.swift
+//  P10-CupcakeCorner
+//
+//  Created by Jonathan Tipton on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct P10_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
