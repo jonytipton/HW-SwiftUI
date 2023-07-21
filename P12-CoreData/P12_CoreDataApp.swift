2b68cd5ef2b00dd03eb1e9f0feffc108c42d815d
Jonathan Tipton <nature_explorer98@outlook.com>
2023-07-21 14:04:52 -0600

    Initial Commit

---
+++ P12-CoreData/P12_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  P12_CoreDataApp.swift
+//  P12-CoreData
+//
+//  Created by Jonathan Tipton on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct P12_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
