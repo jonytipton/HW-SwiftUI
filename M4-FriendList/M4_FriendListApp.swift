2b7a81c9dd6deb6521763810a4e5cccf7418db33
Jonathan Tipton <nature_explorer98@outlook.com>
2023-08-24 15:41:35 -0600

    Initial Commit

---
+++ M4-FriendList/M4_FriendListApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4_FriendListApp.swift
+//  M4-FriendList
+//
+//  Created by Jonathan Tipton on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct M4_FriendListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
